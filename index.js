d7afb8d84ca071fad4325d3877468db4a654eda3
Mercy Manrique <mercymanrique@yahoo.com>
2017-09-28 17:56:54 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+$(function(){
+ 
+ var buzzer = $("#buzzer")[0],
+     count = parseInt($("#num").html()),
+    count2 = parseInt($("#breakNum").html());
+ 
+ $("#m5time").click(function(){
+  if (count > 0){
+   count -= 5;
+   $("num").html(count);
+  }
+  event.preventDefault();
+ });
+ 
+  $("#a5time").click(function(){
+   count += 5;
+   $("num").html(count);
+  event.preventDefault();
+ });
+ 
+ 
+ 
+ 
+ 
+})
