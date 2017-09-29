e8a27b3743b2bc652e32f2988b20ace152ea9fc3
Mercy Manrique <mercymanrique@yahoo.com>
2017-09-29 13:17:53 -0700

    interval functions update

--- index.js
+++ index.js
@@ -1,73 +1,25 @@
-
-$(function() {
-  var buzzer = $("#buzzer")[0],
-    count = parseInt($("#num").html()),
+$(function(){
+ 
+ var buzzer = $("#buzzer")[0],
+     count = parseInt($("#num").html()),
     count2 = parseInt($("#breakNum").html());
-
-  $("#session").hide();
-
-  $("#start").click(function() {
-    var counter = setInterval(timer, 1000);
-    count *= 60;
-    count2 *= 60;
-
-    function timer() {
-      $(
-        "#start, #m5time, #m5break, #a5time, #a5break, #title1, #reset #breakNum, #num"
-      ).hide();
-      $("#session").show();
-      $("#session").html("SESSION TIME: ");
-
-      count -= 1;
-
-      if (count === 0) {
-        buzzer.play();
-        clearInterval(counter);
-        $("#num").hide();
-        var counter2 = setInterval(breakTimer, 1000);
-      }
-
-      $("#num").html(count);
-
-      if (count % 60 >= 10) {
-        $("#num").html(Math.floor(count / 60) + ":" + count % 60);
-      } else {
-        $("#num").html(Math.floor(count / 60) + ":" + "0" + count % 60);
-      }
-
-      function breakTimer() {
-        $("#session").html("BREAK TIME:");
-        $("#breakNum").show();
-
-        count2 -= 1;
-
-        if (count2 === 0) {
-          clearInterval(counter2);
-          buzzer.play();
-          $("#reset").show();
-          $("#breakNum, #session").hide();
-        }
-
-        $("#breakNum").html(count2);
-        if (count2 % 60 >= 10) {
-          $("#breakNum").html(Math.floor(count2 / 60) + ":" + count2 % 60);
-        } else {
-          $("#breakNum").html(
-            Math.floor(count2 / 60) + ":" + "0" + count2 % 60
-          );
-        }
-      }
-    }
-  });
-
-
-
-
+ 
+/* buzzer.play();*/
+ 
+ $("#session").hide();
+ 
+ $("#m5time").click(function(){
+  if (count > 0){
+   count -= 5;
+   $("#num").html(count);
+  }
+  event.preventDefault();
+ });
  
   $("#a5time").click(function(){
    count += 5;
    $("#num").html(count);
- 
+  event.preventDefault();
  });
  
  
